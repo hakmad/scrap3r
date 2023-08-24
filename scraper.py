68264daae355b1a2c64de13135e441dd85877a9d
Jack K <zzz@Jacks-MacBook-Pro.local>
2023-08-24 14:49:45 +0100

    Adding initial file.

---
+++ scraper.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+headers={"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
+url = "---"  
+
+response = requests.get(url, headers=headers)
+
+# Check if the request was successful
+if response.status_code == 200:
+    print("Success")
+    html_content = response.content
+else:
+    print("Failed to fetch the website.")
+    exit()
+
+# Create a BeautifulSoup object to parse the HTML content
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find all the text elements (e.g., paragraphs, headings, etc.) you want to scrape
+#text_elements = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span', class_='article_section'])
+#text_elements = soup.find_all('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span', attrs={"class":"article_section"})
+
+article_section = soup.find('div', class_='article_section')
+text_elements = article_section.find_all('p', 'h1')
+
+# Extract the text from each element and concatenate them into a single string
+scraped_text = ' '.join(element.get_text() for element in text_elements)
+scraped_text = re.sub(' Related Articles.*','',scraped_text)
+print(scraped_text)
