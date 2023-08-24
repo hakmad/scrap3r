93e6e003d86bb6919419b4e5bf4a19ad23be3f42
hakmad <hakmad@fastmail.com>
2023-08-24 16:52:03 +0000

    Added simpler scraper to only get article from scraped text, not extra links

--- scraper.py
+++ scraper.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-headers={"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
-url = "--"  
 
+headers = {"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
+url = "https://www.bleepingcomputer.com/news/security/winrar-zero-day-exploited-since-april-to-hack-trading-accounts/"
+
+# Send request with headers and get response.
 response = requests.get(url, headers=headers)
 
-# Check if the request was successful
+# Check if the request was successful.
 if response.status_code == 200:
     print("Success")
     html_content = response.content
@@ -16,13 +18,14 @@ else:
     exit()
 
 # Create a BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(html_content, 'html.parser')
+soup = BeautifulSoup(html_content, "html.parser")
 
-# Find all the text elements (e.g., paragraphs, headings, etc.) you want to scrape
-article_section = soup.find('div', class_='article_section')
-text_elements = article_section.find_all(['p', 'h1', 'h2', 'h3', 'h4'])
+# Get title and body.
+title = soup.find("div", class_="article_section").find("h1").get_text()
+raw_body = soup.find("div", class_="articleBody").find_all("p", recursive=False)
 
-# Extract the text from each element and concatenate them into a single string - Remove noise with regex.
-scraped_text = ' '.join(element.get_text() for element in text_elements)
-scraped_text = re.sub(' Related Articles.*','',scraped_text)
-print(scraped_text)
+# Join all elements in the body together.
+body = "".join(element.get_text() for element in raw_body)
+
+print(title)
+print(body)
